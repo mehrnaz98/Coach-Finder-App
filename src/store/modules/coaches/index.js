7c3766f12ada3b0f35668a5b478299ad1df95a98
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-22 19:19:08 +0330

    Update index.js

--- src/store/modules/coaches/index.js
+++ src/store/modules/coaches/index.js
@@ -3,7 +3,8 @@ import actions from './actions.js';
 import getters from './getters.js';
 
 export default {
-  state() {
-    mutations, actions, getters;
-  },
+  state() {},
+  mutations,
+  actions,
+  getters,
 };
