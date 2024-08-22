3da498a48c507ea1876571c7d395ad277f6e9aaa
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-22 21:35:26 +0330

    Add hasCoaches as a computed property
    if statement is used to check whether the coaches list is empty. If the list is empty a text instead of an empty list will be displayed.

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,11 +5,12 @@
       <button>Refresh</button>
       <router-link to="/register">Register as Coach</router-link>
     </div>
-    <ul>
+    <ul v-if="hasCoaches">
       <li v-for="coach in filteredCoaches" :key="coach.id">
         {{ coach.firstName }}
       </li>
     </ul>
+    <h3 v-else>No coaches found</h3>
   </section>
 </template>
 
@@ -18,6 +19,9 @@ export default {
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
     },
   },
 };
