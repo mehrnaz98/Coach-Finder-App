a600f903b7559742bdd83519ad618bbbd8c2e92e
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-23 22:50:52 +0330

    Create three inputs with the type of checkbox
    Checkboxes are checked initially.

--- src/components/coaches/CoachFilter.vue
+++ src/components/coaches/CoachFilter.vue
@@ -1,1 +1,17 @@
-<template></template>
+<template>
+  <base-card
+    ><h2>Find Your Coach</h2>
+    <span class="filter-option"
+      ><input type="checkbox" id="frontend" checked />
+      <label for="frontend">Frontend</label>
+    </span>
+    <span class="filter-option">
+      <input type="checkbox" id="backend" checked />
+      <label for="backend">Backend</label>
+    </span>
+    <span class="filter-option"
+      ><input type="checkbox" id="career" checked />
+      <label for="career">Career</label>
+    </span>
+  </base-card>
+</template>
