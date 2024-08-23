723da6005ce5ee2693eb44a0639e775a37ab998d
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-23 18:21:00 +0330

    Add router view for rendering the contact form

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -11,6 +11,7 @@
         <h2>Interested? Reach out now!</h2>
       </header>
       <base-button link :to="contactLink">Contact</base-button>
+      <router-view></router-view>
     </base-card>
   </section>
 </template>
