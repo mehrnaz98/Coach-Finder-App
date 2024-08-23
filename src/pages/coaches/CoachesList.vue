3571e94c541da42a5ef1fb0c96761bf12fc11c5f
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-23 15:19:07 +0330

    Add CSS styling

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -34,3 +34,16 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
