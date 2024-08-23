b1de2344efe0ed01b46e6515b19f994747cebd04
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-23 16:16:40 +0330

    Change Refresh button mode to outline

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,7 +3,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button>Refresh</base-button>
+        <base-button mode="outline">Refresh</base-button>
         <base-button link to="/register">Register as Coach</base-button>
       </div>
       <ul v-if="hasCoaches">
