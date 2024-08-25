33c9b7044acd2f0fb101672cc25b6c604d4fa6bf
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-25 20:37:14 +0330

    Add registerCoach in actions.js
    The data received from coach registration differs from the data in the store; therefore the names should be matched. Save the new data in the coachData constant.

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,1 +1,11 @@
-export default {};
+export default {
+  registerCoach(context, data) {
+    const coachData = {
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas,
+    };
+  },
+};
