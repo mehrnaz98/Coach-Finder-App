4eeb52389a0552b6bd7f20d59d4feb97440f67c2
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-25 16:35:32 +0330

    Import and register CoachForm locally into oachRegistration component

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -3,3 +3,13 @@
     <h2>Register as a coach now!</h2>
   </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
