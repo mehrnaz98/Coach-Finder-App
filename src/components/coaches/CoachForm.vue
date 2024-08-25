09fe0f75c545afa3ecf38a2025404b4b1b214914
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-25 16:19:57 +0330

    Add a form to register as a coach

---
+++ src/components/coaches/CoachForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form>
+    <div class="form-control">
+      <label for="firstName">Firstname</label>
+      <input type="text" id="firstName" />
+    </div>
+    <div class="form-control">
+      <label for="lastName">Lastname</label>
+      <input type="text" id="lastName" />
+    </div>
+    <div class="form-control">
+      <label for="description">Description</label>
+      <textarea id="description" row="5"></textarea>
+    </div>
+    <div class="form-control">
+      <label for="rate">Hourly Rate</label>
+      <input type="number" id="rate" />
+    </div>
+    <div class="form-control">
+      <h3>Areas of Expertise</h3>
+      <div>
+        <input type="checkbox" id="frontend" value="frontend" />
+        <label for="frontend">Frontend Development</label>
+      </div>
+      <div>
+        <input type="checkbox" id="Backend" value="Backend" />
+        <label for="frontend">Backend Development</label>
+      </div>
+      <div>
+        <input type="checkbox" id="career" value="career" />
+        <label for="career">Career Advisory</label>
+      </div>
+    </div>
+  </form>
+</template>
