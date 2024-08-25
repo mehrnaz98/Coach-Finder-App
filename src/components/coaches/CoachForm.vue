cb6288e383fe7ad328459dc3bbf5596630cc55f4
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-25 16:29:24 +0330

    Add v-model to bind the data

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -2,32 +2,32 @@
   <form>
     <div class="form-control">
       <label for="firstName">Firstname</label>
-      <input type="text" id="firstName" />
+      <input type="text" id="firstName" v-model.trim="firstName" />
     </div>
     <div class="form-control">
       <label for="lastName">Lastname</label>
-      <input type="text" id="lastName" />
+      <input type="text" id="lastName" v-model.trim="lastName" />
     </div>
     <div class="form-control">
       <label for="description">Description</label>
-      <textarea id="description" row="5"></textarea>
+      <textarea id="description" row="5" v-model.trim="description"></textarea>
     </div>
     <div class="form-control">
       <label for="rate">Hourly Rate</label>
-      <input type="number" id="rate" />
+      <input type="number" id="rate" v-model.number="rate" />
     </div>
     <div class="form-control">
       <h3>Areas of Expertise</h3>
       <div>
-        <input type="checkbox" id="frontend" value="frontend" />
+        <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
         <label for="frontend">Frontend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="Backend" value="Backend" />
+        <input type="checkbox" id="Backend" value="Backend" v-model="areas" />
         <label for="frontend">Backend Development</label>
       </div>
       <div>
-        <input type="checkbox" id="career" value="career" />
+        <input type="checkbox" id="career" value="career" v-model="areas" />
         <label for="career">Career Advisory</label>
       </div>
     </div>
