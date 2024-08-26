c3ed43e0a4a527beb3e9b308ba526eccd7c9374c
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-26 22:01:17 +0330

    Access the id by context.rootGetters

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,7 +1,7 @@
 export default {
   registerCoach(context, data) {
     const coachData = {
-      id: 'c3',
+      id: context.rootGetters,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
