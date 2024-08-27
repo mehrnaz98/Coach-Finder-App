a7c7585865ba2379fb3d37c2e61b0b1f1c1a09a8
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-28 00:25:22 +0330

    Update CoachForm.vue

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -90,7 +90,7 @@ export default {
         this.firstName.isValid = false;
         this.formIsValid = false;
       }
-      if (this.LastName.val === '') {
+      if (this.lastName.val === '') {
         this.lastName.isValid = false;
         this.formIsValid = false;
       }
