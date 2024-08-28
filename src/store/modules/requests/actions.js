cf9ee912995e34c6f5fbad0fcb1f355440246268
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-29 00:23:19 +0330

    Create new id for every request object

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,6 +1,7 @@
 export default {
   contactCoach(context, payload) {
     const newRequest = {
+      id: new Date().toISOString(),
       coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
