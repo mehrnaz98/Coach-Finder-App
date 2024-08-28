0c7e9b19ddad35134d98e008225e273b93c644c0
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-29 01:03:25 +0330

    Update index.js

--- src/store/modules/requests/index.js
+++ src/store/modules/requests/index.js
@@ -1,5 +1,5 @@
-import mutations from './mutations';
-import actions from './actions';
+import mutations from './mutations.js';
+import actions from './actions.js';
 
 export default {
   namespaced: true,
