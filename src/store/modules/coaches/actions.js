2f5d24f916794b6574e151fb45d1a973cd17f9f6
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-30 23:01:17 +0330

    Update actions.js

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -17,7 +17,7 @@ export default {
       }
     );
 
-    const responseData = await response.json();
+    // const responseData = await response.json();
 
     if (!response.ok) {
       // error ...
