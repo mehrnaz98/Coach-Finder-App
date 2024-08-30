296f851d1b20de96ec5bda6922ca7c39f4679115
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-30 19:19:17 +0330

    Filter the requests
    It is done to see whether the coach id that the request is sent to, matches the id of the coach; therefore, each request will only be shown to the coach that request is meant for.

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,7 +1,7 @@
 export default {
   requests(state, getters, rootState, rootGetters) {
     const coachId = rootGetters.userId;
-    return state.requests;
+    return state.requests.filter((req) => req.coachId === coachId);
   },
   hasRequests(state) {
     return state.requests && state.requests.length > 0;
