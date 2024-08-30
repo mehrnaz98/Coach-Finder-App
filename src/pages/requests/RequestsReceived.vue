861dd3662b86656ac222a1a5bdb1a682df4fd126
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-30 15:33:16 +0330

    Add a paragraph to be displayed whenever no requests were received

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -4,6 +4,8 @@
       <header>
         <h2>Requests Received</h2>
       </header>
+      <ul></ul>
+      <h3>You haven't received any requests yet!</h3>
     </base-card>
   </section>
 </template>
