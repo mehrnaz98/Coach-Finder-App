dbb0068c2cb19d26a99486ac2462d8b604d480dc
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-30 15:33:50 +0330

    Add CSS Styling

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -9,3 +9,20 @@
     </base-card>
   </section>
 </template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
