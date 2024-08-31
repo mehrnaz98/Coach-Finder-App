a0a1c838efbf4fd58aa0a98c3164fc86b01ca1fc
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-31 22:22:05 +0330

    Use try and catch to set the error message

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -80,7 +80,11 @@ export default {
     },
     async loadCoaches() {
       this.isLoading = true;
-      await this.$store.dispatch('coaches/loadCoaches');
+      try {
+        await this.$store.dispatch('coaches/loadCoaches');
+      } catch (error) {
+        this.error = error.message || 'Something went wrong';
+      }
       this.isLoading = false;
     },
   },
