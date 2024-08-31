197a686c72eb24d10b820df3edbb35e3946af748
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-31 15:24:41 +0330

    Import base spinner component is main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue';
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
+import BaseSpinner from './components/ui/BaseSpinner.vue';
 
 const app = createApp(App);
 
