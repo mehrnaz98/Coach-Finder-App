e2ce4e9cfdd2a3121b6a976d0061c5c07b8e16c5
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-31 15:25:26 +0330

    Register base spinner component globally

--- src/main.js
+++ src/main.js
@@ -16,5 +16,6 @@ app.use(store);
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
 app.component('base-badge', BaseBadge);
+app.component('base-spinner', BaseSpinner);
 
 app.mount('#app');
