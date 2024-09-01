65014950bf9236d72ab5dd644a14621e3ef16403
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-01 13:00:19 +0330

    Use fetch method to send the HTTP request
    It is done to store all the requests received. Fire Base automatically generates a new id. the name field in the request holds the generated id; Therefore, the newRequest.id is assigned to the name that is received from the responseData.

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,22 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
       coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
+    const response = await fetch(
+      `https://find-a-coach-app-6891b-default-rtdb.firebaseio./requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    const responseData = await response.json();
+
+    newRequest.id = responseData.name;
+
     context.commit('addRequest', newRequest);
   },
 };
