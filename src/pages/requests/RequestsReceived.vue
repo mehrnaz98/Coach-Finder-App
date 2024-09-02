e470a5f71b04e059b6364be4edc6754409401f12
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-02 17:04:03 +0330

    Update RequestsReceived.vue
    When using a transition, the wrapped component must have only one element.

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,26 +1,28 @@
 <template>
-  <base-dialog
-    :show="!!error"
-    title="An error occured"
-    @close="handleError"
-  ></base-dialog>
-  <section>
-    <base-card>
-      <header>
-        <h2>Requests Received</h2>
-      </header>
-      <base-spinner v-if="isLoading"></base-spinner>
-      <ul v-else-if="hasRequests">
-        <requests-item
-          v-for="req in receivedRequests"
-          :key="req.id"
-          :email="req.userEmail"
-          :message="req.message"
-        ></requests-item>
-      </ul>
-      <h3 v-else>You haven't received any requests yet!</h3>
-    </base-card>
-  </section>
+  <div>
+    <base-dialog
+      :show="!!error"
+      title="An error occured"
+      @close="handleError"
+    ></base-dialog>
+    <section>
+      <base-card>
+        <header>
+          <h2>Requests Received</h2>
+        </header>
+        <base-spinner v-if="isLoading"></base-spinner>
+        <ul v-else-if="hasRequests">
+          <requests-item
+            v-for="req in receivedRequests"
+            :key="req.id"
+            :email="req.userEmail"
+            :message="req.message"
+          ></requests-item>
+        </ul>
+        <h3 v-else>You haven't received any requests yet!</h3>
+      </base-card>
+    </section>
+  </div>
 </template>
 
 <script>
