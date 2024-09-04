e18e1c76dfc9e823c120f2fda1506464a5bba577
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-04 15:59:33 +0330

    Import UserAuth component in router.js

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 
 const router = createRouter({
   history: createWebHistory(),
