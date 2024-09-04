f166a2286e88c2cd33fafb3fba8452beb2a9c071
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-04 15:06:00 +0330

    Add CSS styling

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -12,3 +12,36 @@
     </div>
   </form>
 </template>
+
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+</style>
