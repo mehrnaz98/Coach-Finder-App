512d87d1283d45f512cc4ddff8b476f47fae8715
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-04 15:09:31 +0330

    Wrap the form by base card component

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -1,16 +1,18 @@
 <template>
-  <form>
-    <div class="form-control">
-      <label for="email">Email</label>
-      <input type="email" id="email" />
-    </div>
-    <div class="form-control">
-      <label for="password">Password</label>
-      <input type="password" id="password" />
-      <base-button>Login</base-button>
-      <base-button type="button" mode="flat">Signup instead</base-button>
-    </div>
-  </form>
+  <base-card>
+    <form>
+      <div class="form-control">
+        <label for="email">Email</label>
+        <input type="email" id="email" />
+      </div>
+      <div class="form-control">
+        <label for="password">Password</label>
+        <input type="password" id="password" />
+        <base-button>Login</base-button>
+        <base-button type="button" mode="flat">Signup instead</base-button>
+      </div>
+    </form>
+  </base-card>
 </template>
 
 <style scoped>
