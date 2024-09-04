4841f2b26633db81de7ccbee2a34bb3b57dafcdb
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-04 14:54:22 +0330

    Add a form with two divs with the id form-control for signing up

---
+++ src/pages/auth/UserAuth.vue
@@ -0,0 +1,12 @@
+<template>
+  <form>
+    <div class="form-control">
+      <label for="email">Email</label>
+      <input type="email" id="email" />
+    </div>
+    <div class="form-control">
+      <label for="password">Password</label>
+      <input type="password" id="password" />
+    </div>
+  </form>
+</template>
