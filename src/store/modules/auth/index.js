b91bbabebe93c2083356a347709bd5791002e613
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-06 13:31:56 +0330

    Update index.js

--- src/store/modules/auth/index.js
+++ src/store/modules/auth/index.js
@@ -1,7 +1,14 @@
+import mutations from '../coaches/mutations';
+import actions from '../coaches/actions';
+import getters from './getters';
+
 export default {
   state() {
     return {
       userId: 'c3',
     };
   },
+  mutations,
+  actions,
+  getters,
 };
