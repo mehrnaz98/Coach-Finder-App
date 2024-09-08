71aac332052cc83813e6369b018dc73b2a0de191
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-08 22:07:29 +0330

    Add a signup action
    It sends the user's email and password to the server. It also handles errors.

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,1 +1,27 @@
-export default {};
+export default {
+  async signup(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyAl-kmtImlWFBQyKK_tGG3p6jIAY2LyRxs',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || 'Failed to authenticate.'
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+  },
+};
