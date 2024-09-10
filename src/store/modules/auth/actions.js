cb133fee2a6eb8206226ae187917cf99529d2038
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-11 00:26:03 +0330

    Update actions.js

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -32,7 +32,6 @@ export default {
 
   async signup(context, payload) {
     const response = await fetch(
-      //'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyAl-kmtImlWFBQyKK_tGG3p6jIAY2LyRxs',
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAl-kmtImlWFBQyKK_tGG3p6jIAY2LyRxs',
       {
         method: 'POST',
