6201422f8f66dbd4361b17c1e52dfc0bf8289439
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-11 00:38:50 +0330

    Update actions.js

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -9,7 +9,7 @@ export default {
       areas: data.areas,
     };
 
-    const token = context.getters.token;
+    const token = context.rootGetters.token;
 
     const response = await fetch(
       `https://find-a-coach-app-6891b-default-rtdb.firebaseio.com/coaches/${userId}.json?auth=` +
