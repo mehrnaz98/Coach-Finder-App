587eb154abf2c684dae5b3fec0c472c035901487
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-12 00:09:29 +0330

    Add a login button to be displayed if the user is not logged in

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -14,6 +14,7 @@
           <base-button mode="outline" @click="loadCoaches(true)"
             >Refresh</base-button
           >
+          <base-button link to="/auth" v-if="!isLoggedIn">Login</base-button>
           <base-button
             v-if="!isLoggedIn && !isCoach && !isLoading"
             link
