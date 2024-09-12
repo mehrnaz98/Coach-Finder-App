77a237f406f3e34554dea214505fbc61e51b4488
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-12 18:30:36 +0330

    Update mutations.js
    Merge existing coaches with fetched data.

--- src/store/modules/coaches/mutations.js
+++ src/store/modules/coaches/mutations.js
@@ -3,7 +3,7 @@ export default {
     state.coaches.push(payload);
   },
   setCoaches(state, payload) {
-    state.coaches = payload;
+    state.coaches = [...state.coaches, ...payload];
   },
   setFetchTimestamp(state) {
     state.lastFetch = new Date().getTime();
