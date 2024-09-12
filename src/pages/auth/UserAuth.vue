64419b69d9381108923e6377d056a2d8a8951a46
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-12 14:38:07 +0330

    Update UserAuth.vue
    There might not always be a redirect; in that case the coaches page will be displayed.

--- src/pages/auth/UserAuth.vue
+++ src/pages/auth/UserAuth.vue
@@ -82,8 +82,8 @@ export default {
         } else {
           await this.$store.dispatch('signup', actionPayload);
         }
-        const redirectUrl = '/' + this.$route.query.redirect;
-        this.$router.replace('/coaches');
+        const redirectUrl = '/' + (this.$route.query.redirect || 'coaches');
+        this.$router.replace(redirectUrl);
       } catch (err) {
         this.error = err.message || 'Failed to authentoicate, try later.';
       }
