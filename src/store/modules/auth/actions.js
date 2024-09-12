741b6424088c36ea2dab42af29ac9de49af5aa5e
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-12 15:10:57 +0330

    Create an auth action

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -60,6 +60,39 @@ export default {
       tokenExpiration: responseData.expiresIn,
     });
   },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAl-kmtImlWFBQyKK_tGG3p6jIAY2LyRxs';
+
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAl-kmtImlWFBQyKK_tGG3p6jIAY2LyRxs';
+    }
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(
+        responseData.message || 'Failed to authenticate. Check your login data.'
+      );
+      throw error;
+    }
+
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
   logout(context) {
     context.commit('setUser', {
       userId: null,
