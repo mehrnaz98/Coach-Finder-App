d244ac464cfba1431bc97096066a01437c1c2497
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-09-12 14:28:01 +0330

    Update CoachesList.vue

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -14,7 +14,9 @@
           <base-button mode="outline" @click="loadCoaches(true)"
             >Refresh</base-button
           >
-          <base-button link to="/auth" v-if="!isLoggedIn">Login</base-button>
+          <base-button link to="/auth" v-if="!isLoggedIn"
+            >Login to Register as coach</base-button
+          >
           <base-button
             v-if="isLoggedIn && !isCoach && !isLoading"
             link
